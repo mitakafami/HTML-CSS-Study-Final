91f2352de770be6d954da47421c9c2c372a35164
mitakafami <102894287+mitakafami@users.noreply.github.com>
2023-11-15 18:35:12 +0400

    add burger

---
+++ script.js
@@ -0,0 +1,21 @@
+let burger = document.querySelector(".burger");
+let xMark = document.querySelector(".mark");
+let mobileMenu = document.querySelector(".header_nav");
+let currencyLanguage = document.querySelector(".currency_language")
+
+burger.addEventListener("click", () => {
+    mobileMenu.classList.add("show");
+    currencyLanguage.classList.add("show_currency")
+    burger.style.display = "none";
+    xMark.style.display = "block";
+} )
+
+xMark.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+    currencyLanguage.classList.remove("show")
+    burger.style.display = "block";
+    xMark.style.display = "none";
+  }
+
+);
+
